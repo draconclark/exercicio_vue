9db95449aab73992d12cdb48adbc704356d65acb
draconclark <kelvinlagoa3@gmail.com>
2023-10-03 16:01:17 -0300

    commit

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,12 @@
+<template>
+    <header class="d-flex align-items-center justify-content-center">
+        <img class="header-img h-100 me-3" src="./calculator.png" alt="Calculadora">
+        <h1 class="text-center mt-3">Calculadora aritmética</h1>
+    </header>
+</template>
+
+<style scoped>
+.header-img {
+    width: 56px;
+}
+</style>
